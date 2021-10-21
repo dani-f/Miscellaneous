ff8ed2fbb2de9a764f7588327e569ef1b2e0f534
Daniel F <80155259+dani-f@users.noreply.github.com>
2021-10-21 17:20:31 +0200

    Update visualize_data_in_excel.R

--- visualize_data_in_excel.R
+++ visualize_data_in_excel.R
@@ -1,14 +1,14 @@
 ######################
 #
 # Visualize data in Excel
-# A convenient helper function to quickly visualize your data in Excel (or in this case .csv)
+# A convenient helper function to quickly visualize your data in Excel (or, in this case, .csv)
 #
 # Seen here: https://twitter.com/brodriguesco/status/1447468259725434886
 # 
 ######################
 
 excel_visualize <- function(.data){
-  # consider using if(interactive())... to avoid unwanted file executions
+  # consider using if(interactive())... to avoid undesired file executions
   tmp <- tempfile(fileext = ".csv")
   write.csv2(.data, tmp)
   file.show(tmp)
