2ddaacd06bcf9bdbdaf6d896fc9bd025f27f2509
dani-f <da.gegen@posteo.de>
2026-04-01 14:07:12 +0200

    Fix 403 error

--- 3_my_football_teams.R
+++ 3_my_football_teams.R
@@ -19,8 +19,11 @@ my_football_teams <- function(my_team){
     GET(url,
         # Add headers to mimic real user request
         add_headers(
-          "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0 Safari/537.36",
+          "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+          "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
           "Accept-Language" = "de-DE,de;q=0.9,en;q=0.8",
+          "Connection" = "keep-alive",
+          "Upgrade-Insecure-Requests" = "1",
           "Referer" = "https://www.kicker.de/"
           )))
   
